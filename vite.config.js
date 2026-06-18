4064831d24b4dc9fd603d33a6f471be4e9b0bf66
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-06-19 00:11:13 +0500

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/UniversityProjectMern/',   // 👈 add this
   plugins: [
     react(),
     tailwindcss(),
